2928025fb367b1c628de661aac1f29de1f88bc29
dDKILAA <demetrekiladze895@gmail.com>
2025-01-22 22:27:08 +0400

    Update Demetre_kiladze_16.py

--- Demetre_kiladze_16.py
+++ Demetre_kiladze_16.py
@@ -1,55 +1,30 @@
 class Student:
-    def __init__(self, first_name, last_name, personal_id, birth_year, grades):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, name, lastname, personal_id, birth_year, grades):
+        self.name = name
+        self.lastname = lastname
         self.personal_id = personal_id
         self.birth_year = birth_year
         self.grades = grades
 
     def __str__(self):
-        return (
-            f"Student:: {self.first_name} {self.last_name}\n"
-            f"Personal ID: {self.personal_id}\n"
-            f" Birth Year: {self.birth_year}\n"
-            f"Grades: {self.grades}"
-        )
+        return f"name : {self.name } ,\n lastname : {self.lastname},\n personal_id : {self.personal_id} \n birth_year: {self.birth_year}\n grades: {self.grades}"
 
     def years_until_18(self):
         current_year = 2025
         age = current_year - self.birth_year
         if age >= 18:
-            return 0
+            return "*Already was!*"
         return 18 - age
 
+student1 = Student("nana", "varlamishvili" ,"0121312" , 2008, {"math": 95, "programming": 100})
+student2 = Student("jemala", "zangidze" ,"3562234" , 2003 , {"math": 0, "programming": 101})
+student3 = Student("nugzari", "pavlovic" ,"777217" , 2009 , {"math": 0, "programming": 0})
 
-student1 = Student(
-    first_name="Nana",
-    last_name="katamadze",
-    personal_id="01012000001",
-    birth_year=2010,
-    grades={"math": 95, "programming": 100}
-)
+print(student1, "\n")
+print(f"will be 18 in {student1.years_until_18()} years")
 
-student2 = Student(
-    first_name="Giorgi",
-    last_name="Jajanidze",
-    personal_id="02022000002",
-    birth_year=2008,
-    grades={"math": 88, "programming": 90}
-)
+print(student2,"\n" )
+print(f"Giorgi will be 18 in  {student2.years_until_18()} years")
 
-student3 = Student(
-    first_name="Jemala",
-    last_name="Kiladze",
-    personal_id="03032000003",
-    birth_year=2015,
-    grades={"math": 100, "programming": 99}
-)
-print(student1)
-print(f"Nana will be 18 in {student1.years_until_18()} years.\n")
-
-print(student2)
-print(f"Giorgi will be 18 in  {student2.years_until_18()} years.\n")
-
-print(student3)
-print(f"Jemala will be 18 in  {student3.years_until_18()} years.\n")
+print(student3, "\n")
+print(f"Jemala will be 18 in  {student3.years_until_18()} years")
