24ae9db4311881b84bfa3f9b8f67f6a4613710c2
dDKILAA <demetrekiladze895@gmail.com>
2025-01-29 19:04:32 +0400

    Update demetre_kiladze_18.py

--- demetre_kiladze_18.py
+++ demetre_kiladze_18.py
@@ -5,13 +5,13 @@ class triangle :
         self.__b  = b
         self.__c = c
 
-    def perimeter(self) :
+    def get_perimeter(self) :
         return self.__a + self.__b +self.__c
     
-    def area(self) :
+    def get_area(self) :
         s = (self.__a + self.__b +self.__c ) / 2
         return math.sqrt(s *(s - self.__a) * (s - self.__b) * (s - self.__c))
     
 triangle_1 = triangle (5,6,7)
-print ("triangle_1 perimneter ", triangle_1.perimeter())
-print ("triangle_1 area ", triangle_1.area())
+print ("triangle_1 perimneter ", triangle_1.get_perimeter())
+print ("triangle_1 area ", triangle_1.get_area())
