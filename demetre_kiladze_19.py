44b2cc479272f3bf80fbf5cd4d3b8dced6e2f9a8
dDKILAA <demetrekiladze895@gmail.com>
2025-02-04 23:03:41 +0400

    davaleba 19

--- demetre_kiladze_19.py
+++ demetre_kiladze_19.py
@@ -8,7 +8,8 @@ class animal :
         print (f"Hello my name is {self.name} with color {self.color} and i am an animal")
 
 class dog(animal) :
-    pass
+    def say_hi(self) :
+      print (f"Hello my name is {self.name} with color {self.color} and i am a Dog")
 
 animal_1 = dog("Jemala" , "zangi" )
 animal_1.say_hi()
